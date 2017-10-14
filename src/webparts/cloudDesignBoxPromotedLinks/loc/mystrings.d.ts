8cef6b78b57112bdbe8fd8dfab793f6c18467787
Tony Phillips <Tony.Phillips@clouddesignbox.co.uk>
2017-10-14 10:52:17 +0100

    First commit

---
+++ src/webparts/cloudDesignBoxPromotedLinks/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICloudDesignBoxPromotedLinksWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CloudDesignBoxPromotedLinksWebPartStrings' {
+  const strings: ICloudDesignBoxPromotedLinksWebPartStrings;
+  export = strings;
+}
