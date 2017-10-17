4a1142571a820847ee74d5c8b3b4b866e4144a90
Tony Phillips <Tony.Phillips@clouddesignbox.co.uk>
2017-10-17 15:00:08 +0100

    Updated version number

---
+++ Installation/Installation/version 2/CDBPromotedLinks/cdbpromotedlinks-clouddesignboxpromotedlinkswebpartstrings_en-us_765458216e451186e10982218c7e2d58.js
@@ -0,0 +1,11 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Promoted Links Web Part by Cloud Design Box Ltd https://www.clouddesignbox.co.uk",
+    "BasicGroupName": "List",
+    "AdvancedGroupName": "Advanced Settings",
+    "ImageLibraryFieldLabel": "Promoted Links List Name",
+    "TileColour": "Tile Background Colour",
+    "TileAnimation": "Hover Animation",
+    "BackgroundSizeFieldLabel": "Background Image Size"
+  }
+});
