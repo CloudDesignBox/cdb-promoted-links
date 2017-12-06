951b12175b7d3f6df1967a96f7cf34783554f45b
Tony Phillips <Tony.Phillips@clouddesignbox.co.uk>
2017-12-06 09:42:35 +0000

    New version and rewrite of web part with new functionality
    Now includes:
    Show and rename web part title option
    turn on and off hover animation
    improved animation to stop "bouncing"
    option to use theme colour of SharePoint site
    tile width option
    option to show the add new link button (we will enhance this in future
    version to detect user permission level and show accordingly)

--- README.md
+++ README.md
@@ -12,6 +12,8 @@
 <p>-	Upload folder “CDBPromotedLinks” (including the folder itself) into the SiteAssets library at the root site collection (e.g. contoso.sharepoint.com/siteassets).</p>
 <p>-	Upload the “cloud-design-box-promoted-links.sppkg” file to the AppCatalog, tick the box to make available on all site collections.
 </p>
+<b>Updating instructions</b>
+<p>Follow the installation instructions above but replace all the files and check in the sppkg file after upload. Please note that upgrading to version 3 will require you to remove and add the web parts onto the page (due to the large configuration and rewrite).</p>
 
 <h1>Working with the code</h1>
 ### Building the code
--- src/webparts/cloudDesignBoxPromotedLinks/CloudDesignBoxPromotedLinksWebPart.ts
+++ src/webparts/cloudDesignBoxPromotedLinks/CloudDesignBoxPromotedLinksWebPart.ts
@@ -18,19 +18,25 @@ import {
 //import colour picker library - third party library https://oliviercc.github.io/sp-client-custom-fields
 import { PropertyFieldColorPicker } from 'sp-client-custom-fields/lib/PropertyFieldColorPicker';
 
+import centralstyles from '../../sharedresources/centralstyles.module.scss';
 import styles from './CloudDesignBoxPromotedLinksWebPart.module.scss';
 import * as strings from 'CloudDesignBoxPromotedLinksWebPartStrings';
 
 //load jquery
 import * as jQuery from 'jquery';
 
-export interface ICloudDesignBoxPromotedLinksWebPartProps {
+export interface IPromotedLinksByCloudDesignBoxWebPartProps {
   description: string;
   imagelibraryname: string;
   tilecolour: string;
   tileanimation: boolean;
   Color: string;
   backgroundsize: string;
+  showtitle: boolean;
+  webparttitle: string;
+  setwidth: string;
+  themecolour:boolean;
+  showaddbutton:boolean;
 }
 //promoted lists to populate properties
 export interface ISPListofLists {
@@ -58,7 +64,7 @@ export interface IPromotedListLists {
   text: string;
 }
 
-export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClientSideWebPart<ICloudDesignBoxPromotedLinksWebPartProps> {
+export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClientSideWebPart<IPromotedLinksByCloudDesignBoxWebPartProps> {
   /*Guidance on dynamically loading options into properties - http://www.sharepointnutsandbolts.com/2016/09/sharepoint-framework-spfx-web-part-properties-dynamic-dropdown.html*/
   private promotedLinksDropdown: IPromotedListLists[];
   private promotedlistsLoaded: boolean;
@@ -102,7 +108,7 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
       if (items.length > 0){
         //loop through items and add to html
         items.forEach((item: ISPList) => {
-          let cdbcolour: string = this.properties.tilecolour;
+          let cdbcolour: string = `background-color:${this.properties.tilecolour};`;
           let cdblaunchbeh: string = "";
           let cdbbackgimage:string = "";
           let cdbdescription:string = "Click here";
@@ -120,21 +126,38 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
           if(item.Description != null){
             cdbdescription=item.Description;
           }
-          html+=`
-          <div class="${styles.tiles}">
-            <div class="${styles.tilecontent} ${styles.tpmouse}" style="background-color:${cdbcolour};background-image:url('${cdbbackgimage}');background-size:${this.properties.backgroundsize};position:relative;" onclick="${cdblaunchbeh}">
-              <div class="${styles.cdbdescholder}">
-                <div class="${styles.cdbdescholdertitle}"><span>${item.Title}</span></div>
-                <div class="${styles.cdbdescholderdesc}"><span>${cdbdescription}</span></div>
+          //if theming is used, don't set bg colour
+          if(this.properties.themecolour==true){
+            cdbcolour=``;
+          }
+          html+=`<div style="width:${this.properties.setwidth};display:inline-block;" class="${styles.mobiletile}">
+            <div>
+              <div class="${styles.tiles}">
+                <div class="${styles.tilecontent} ${styles.tpmouse}" style="${cdbcolour}background-image:url('${cdbbackgimage}');background-size:${this.properties.backgroundsize};position:relative;" onclick="${cdblaunchbeh}">
+                  <div class="${styles.cdbdescholder}">
+                    <div class="${styles.cdbdescholdertitle}"><span>${item.Title}</span></div>
+                    <div class="${styles.cdbdescholderdesc}"><span>${cdbdescription}</span></div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>`;
         });
+        //add new link button
+        if(this.properties.showaddbutton == true){
+          html += `<div style="width:${this.properties.setwidth};display:inline-block;" class="${styles.mobiletile}">
+            <div>
+              <div class="${styles.tiles}">
+                <div class="${centralstyles.cdbplusbuttoncontainer}"><a href="${escape(this.context.pageContext.web.absoluteUrl)}/Lists/${this.properties.imagelibraryname}/NewForm.aspx?Source=${encodeURIComponent(window.location.href).replace(".","%2E")}" class="${centralstyles.cdbplusbuttonleft}">&#43;</a><span class="${centralstyles.cdbclear}">add</span></div>
+              </div>
+            </div>
+          </div>`;
+        }
       }else{
-        html = `There are no links in this list.<br /><a href="${escape(this.context.pageContext.web.absoluteUrl)}/Lists/Promoted%20Links/AllItems.aspx" class="${styles.cdbbutton}">Add Links</a>`;                
+        html = `There are no links in this list.<br />`;                
       }
     }else{
-        html = `This list does not exist. <br /><a href="${escape(this.context.pageContext.web.absoluteUrl)}/_layouts/15/viewlsts.aspx" class="${styles.cdbbutton}">Manage Lists</a>`;
+        html = `Open the web part properties and select a list that already exists on the site or create a new promoted links list on this site using the link below.<br /><a href="${escape(this.context.pageContext.web.absoluteUrl)}/_layouts/15/viewlsts.aspx" class="${centralstyles.cdbbutton}">Manage Lists</a>`;
     }
 
     //render results
@@ -142,19 +165,22 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
     listContainer.innerHTML = html;
 
     //load jQuery features
-    jQuery(document).ready(function() {
+    jQuery(() => {
       jQuery(`.${styles.cdbsubjecttiles}`).show();
     });
+
     //only load animation if prop is true
     if(this.properties.tileanimation == true)
     {
-      jQuery( document ).ready(function() {
+      jQuery(() => {
         jQuery("." + styles.tilecontent)
           .mouseenter(function() {
-            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).slideToggle("slow");
+            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).stop();
+            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).slideToggle("fast");
         })
           .mouseleave(function() {
-            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).slideToggle("slow");
+            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).stop();
+            jQuery(this).children("." + styles.cdbdescholder).children("." + styles.cdbdescholderdesc).slideToggle("fast");
         });
       });
     }
@@ -163,14 +189,25 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
   }
 
   public render(): void {
-    this.domElement.innerHTML = `
+    let temphtml:string = `
       <div class="${styles.cloudDesignBoxPromotedLinks}">
-        <div class="${styles.cdbsubjecttiles}"><div><div class="${styles.actionicons} ${styles.row}" id="${styles.row}">
-          <!--jquery to insert links here-->
-        </div><span class="${styles.cdbclear}"></span></div></div>
+        <div class="${styles.cdbsubjecttiles}">
+          <div>`;
+              //show header if true
+    if(this.properties.showtitle == true){
+      temphtml += `<div class="${centralstyles.heading}">${this.properties.webparttitle}</div>`;
+    }
+    temphtml += `
+            <div class="${styles.actionicons} ${styles.row}" id="${styles.row}">
+              <!--jquery to insert links here-->
+              <span class="${styles.cdbclear}"></span>
+            </div>
+          </div>
+        </div>
       </div>
-      
       `;
+      
+      this.domElement.innerHTML = temphtml;
 
       //render promoted links data
       this._getListData()
@@ -210,14 +247,27 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
                 PropertyPaneDropdown('imagelibraryname', {
                   label: strings.ImageLibraryFieldLabel,
                   options: this.promotedLinksDropdown
+                }),
+                PropertyPaneToggle('showtitle', {
+                  label: "Show Title",
+                  onText: 'On',
+                  offText: 'Off'
+                }),
+                PropertyPaneTextField('webparttitle', {
+                  label: "Web Part Title"
                 })
               ]
             },
             {
-              groupName: strings.AdvancedGroupName,
+              groupName: "Colour and Animation",
               groupFields: [
                 PropertyPaneToggle('tileanimation', {
                   label: strings.TileAnimation,
+                  onText: 'On',
+                  offText: 'Off'
+                }),
+                PropertyPaneToggle('themecolour', {
+                  label: "Use Theme for Tile Background Colour (not colour below)?",
                   onText: 'On',
                   offText: 'Off'
                 }),
@@ -231,16 +281,38 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
                   onGetErrorMessage: null,
                   deferredValidationTime: 0,
                   key: 'colorFieldId'
-               }),
-               PropertyPaneDropdown('backgroundsize', {
-                label: strings.BackgroundSizeFieldLabel,
-                options: [
-                  { key: 'cover', text: 'Cover' },
-                  { key: 'auto', text: 'Auto' },
-                  { key: '100%', text: '100%' },
-                  { key: '50%', text: '50%' },
-                ]
-              })
+               })
+              ]
+            },
+            {
+              groupName: "Size",
+              groupFields: [
+                PropertyPaneDropdown('backgroundsize', {
+                  label: strings.BackgroundSizeFieldLabel,
+                  options: [
+                    { key: 'cover', text: 'cover' },
+                    { key: 'auto', text: 'auto' },
+                    { key: '100%', text: '100%' },
+                    { key: '50%', text: '50%' },
+                    { key: '30%', text: '30%' },
+                    { key: '27%', text: '27%' }
+                  ]
+                }),
+                PropertyPaneDropdown('setwidth', {
+                  label: "Tile Width - desktop view only",
+                  options: [
+                    { key: '150px', text: '150px' },
+                    { key: '50%', text: '50% - 2 in a row' },
+                    { key: '33%', text: '33% - 3 in a row' },
+                    { key: '25%', text: '25% - 4 in a row' },
+                    { key: '20%', text: '20% - 5 in a row' }
+                  ]
+                }),
+                PropertyPaneToggle('showaddbutton', {
+                  label: "Show add new link button?",
+                  onText: 'On',
+                  offText: 'Off'
+                })
               ]
             }
           ]
@@ -251,7 +323,3 @@ export default class CloudDesignBoxPromotedLinksWebPartWebPart extends BaseClien
 }
 
 
-// To do:
-// option to pick backgorund size
-// test everything
-// create a new promoted list if it doesnt exist?
